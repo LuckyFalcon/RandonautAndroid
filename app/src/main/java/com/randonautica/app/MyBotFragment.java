a6ea0eb4fb26123b7fb47a96e36e973af7c2996d
LuckyFalcon <davidfalcon518@gmail.com>
2020-01-30 00:39:04 +0100

    Webview allow returning

--- app/src/main/java/com/randonautica/app/MyBotFragment.java
+++ app/src/main/java/com/randonautica/app/MyBotFragment.java
@@ -1,7 +1,9 @@
 package com.randonautica.app;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.GeolocationPermissions;
@@ -23,7 +25,7 @@ public class MyBotFragment extends Fragment {
             view = inflater.inflate(R.layout.fragment_my_bot, container, false);
 
             //Set webView
-            WebView botWebView = (WebView) view.findViewById(R.id.botWebview);
+            final WebView botWebView = (WebView) view.findViewById(R.id.botWebview);
             botWebView.setWebViewClient(new WebViewClient());
 
             //Set webView settings
@@ -42,6 +44,20 @@ public class MyBotFragment extends Fragment {
             //Set webView url
             botWebView.loadUrl("https://bot.randonauts.com/");
 
+            botWebView.setOnKeyListener(new View.OnKeyListener(){
+
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK
+                            && event.getAction() == MotionEvent.ACTION_UP
+                            && botWebView.canGoBack()) {
+                        botWebView.goBack();
+                        return true;
+                    }
+
+                    return false;
+                }
+
+            });
         }
 
         //Set title of screen
@@ -49,6 +65,7 @@ public class MyBotFragment extends Fragment {
 
         return view;
     }
+
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
