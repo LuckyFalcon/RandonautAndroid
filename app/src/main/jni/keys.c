5884845be08421cb4b65416bee76e5333ee3b93d
LuckyFalcon <davidfalcon518@gmail.com>
2020-05-17 03:24:35 +0200

    api2.randonauts.com

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -5,7 +5,6 @@ Java_com_randonautica_app_RandonautFragment_getBaseApi(JNIEnv *env, jobject inst
 
 
     //https://Api2
-    return (*env)->NewStringUTF(env, "aHR0cHM6Ly9yYW5kb25hdXRpY2EuYXp1cmUtYXBpLm5ldC8=");
-
+    return (*env)->NewStringUTF(env, "aHR0cHM6Ly9hcGkyLnJhbmRvbmF1dHMuY29tLw==");
 
 }
